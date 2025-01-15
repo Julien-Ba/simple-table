519d8768c876003550f150afb3ab13026a227b17
julien-ba <julienbarbier42@gmail.com>
2025-01-15 15:49:44 +0100

    fix: broken module

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -21,17 +22,43 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+    ],
     resolve: {
         extensions: ['.js', '.jsx', '.scss'],
     },
     externals: {
-        react: 'react',
-        'react-dom': 'react-dom',
-        'prop-types': 'prop-types',
-        'str-case-converter': 'str-case-converter',
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react',
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom',
+        },
+        'prop-types': {
+            root: 'PropTypes',
+            commonjs2: 'prop-types',
+            commonjs: 'prop-types',
+            amd: 'prop-types',
+        },
+        'str-case-converter': {
+            root: 'StrCaseConverter',
+            commonjs2: 'str-case-converter',
+            commonjs: 'str-case-converter',
+            amd: 'str-case-converter',
+        },
     },
+    mode: 'production',
 };
